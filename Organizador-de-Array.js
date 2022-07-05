56c41f7ddc74c985a78ad3179fd262cb60cf9f0f
Fernando-Filho <fernando.fs.filhorn@gmail.com>
2022-07-05 17:40:15 -0300

    atualização do código, mas ainda não está completo

--- Organizador-de-Array.js
+++ Organizador-de-Array.js
@@ -1,13 +1,13 @@
 var tabela = [9,10,3,5,15]
-var a = 0
-var b = 1
 for (c = 0; c <tabela.length; c++){
-    a = tabela[c]
-    b = tabela[c+1]
-    for(b;b <=tabela.length; b++){
-        tabela[c] = b
-        tabela[c+1] = a
+    var a = c
+    for(b = c+1;b <=tabela.length; b++){
+        if(a > b){
+            tabela[c] = b
+            tabela[c+1] = a
+        }
     }
 
+    console.log(tabela)
 }
-console.log(tabela)
+console.log(tabela[c+1] = a)
